8e1a855d5f0b67ade0e93c691d9493e358831c00
dMinsz <dsminzks@gmail.com>
2023-08-31 15:49:33 +0900

    test: Test Wheel Interface change

--- Assets/Scripts/WheelChair/WheelInteractable.cs
+++ Assets/Scripts/WheelChair/WheelInteractable.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class WheelInteractable : XRBaseInteractable
+public class WheelInteractable : XRGrabInteractable
 {
     Rigidbody m_Rigidbody;
 
