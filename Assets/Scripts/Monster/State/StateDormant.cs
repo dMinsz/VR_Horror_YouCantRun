257b68b1f0d89f1cfe3dae26fa77ab499bda701d
인권 백 <titanbaek91@gmail.com>
2023-09-05 03:41:20 +0900

    chore : Source code fixes and additions to add mannequin state animations
    마네킹 거리별,상태별 애니메이션 추가로 인한 소스 추가 및 수정

--- Assets/Scripts/Monster/State/StateDormant.cs
+++ Assets/Scripts/Monster/State/StateDormant.cs
@@ -17,6 +17,12 @@ public class StateDorant : MonsterStateBase<Mannequin>
     {
         recognizeCount = 0f;
         owner.Agent.speed = 0f;
+        MannequinIdlePose();
+    }
+
+    public void MannequinIdlePose()
+    {
+        owner.Animator.SetTrigger("Idle");
     }
 
     public override void Exit()
@@ -43,15 +49,11 @@ public class StateDorant : MonsterStateBase<Mannequin>
     public void WaitPlayer()
     {
         // 1. 범위에 들어옴
-        Collider[] colliders = owner.PlayerInSensetiveRange();
-        if (colliders.Length > 0)
+        if (owner.PlayerInColliderRange(owner.SenseRange).Length > 0)
         {
-            foreach (Collider collider in colliders)
-            {
-                recognizeCount += Time.deltaTime;
-                if (recognizeCount >= 5f)
-                    owner.ChangeState(Mannequin_State.Chase);
-            }
+            recognizeCount += Time.deltaTime;
+            if (recognizeCount >= 5f)
+                owner.ChangeState(Mannequin_State.Chase);
         } else
         {
             recognizeCount = 0f;
