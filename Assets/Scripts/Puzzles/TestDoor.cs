f407b6b18e95bfff860f8e33d1e6c712182322ba
dMinsz <dsminzks@gmail.com>
2023-09-07 20:26:55 +0900

    test: Map Edited

--- Assets/Scripts/Puzzles/TestDoor.cs
+++ Assets/Scripts/Puzzles/TestDoor.cs
@@ -6,10 +6,13 @@ public class TestDoor : MonoBehaviour
     private Rigidbody rb;
     private HingeJoint joint;
 
+    private Collider coll;
+    public float MaxAngle = 120f;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         joint = GetComponent<HingeJoint>();
+        coll = GetComponent<Collider>();
     }
 
     private void Start()
@@ -22,8 +25,9 @@ public class TestDoor : MonoBehaviour
     {
         while (true)
         {
-            if (joint.angle == 120 || joint.angle == -120)
+            if (joint.angle >= MaxAngle || joint.angle <= -MaxAngle)
             {
+                coll.enabled = false;
                 rb.isKinematic = true;
 
                 break;
