8646421de0961a58533c8bcb123b22236205ca73
인권 백 <baek626@gmail.com>
2023-09-11 13:55:21 +0900

    chore : Modifying the Player Declaration
    플레이어 선언부 수정

--- Assets/Scripts/MonsterDetecting/MonsterDetecting.cs
+++ Assets/Scripts/MonsterDetecting/MonsterDetecting.cs
@@ -15,18 +15,25 @@ public class MonsterDetecting : MonoBehaviour
 
     private void Awake()
     {
-        cam = Camera.main;
         mannequin = GetComponentInParent<Mannequin>();
         coll = GetComponentInParent<Collider>();
+    }
+
+    private void Start()
+    {
+        cam = Camera.main;
     }
 
     void LateUpdate()
     {
         // ī�޶� �þ� ���� ���� Collider�� �����ȴٸ�
+        if (cam == null)
+            cam = Camera.main;
         cameraFrustum = GeometryUtility.CalculateFrustumPlanes(cam);
+
         if (GeometryUtility.TestPlanesAABB(cameraFrustum, GetComponent<Collider>().bounds))
         {
-            if (Physics.Linecast(transform.position, cam.transform.position, out var hit, obstacleMask)) // �� ������Ʈ���� ī�޶� ��������, distToTarget ���� RayCast ��� 
+            if (Physics.Linecast(transform.position, cam.transform.position, out var hit, obstacleMask)) // �� ������Ʈ���� ī�޶� ��������, distToTarget ���� RayCast ���?
             {
                 if (mannequin.CurState != Mannequin_State.Dormant)
                     mannequin.MannequinBecameInvisible();
