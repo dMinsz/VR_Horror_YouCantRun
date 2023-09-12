f2b07eb76b770250436eaabd5fd1d7ac8f8dbd48
우창민 <rebuilding33@gmail.com>
2023-09-12 16:57:49 +0900

    feat : add stopper in bath prefab
    (stopper추가)

--- Assets/Scripts/TEST/wcm/DrainWater.cs
+++ Assets/Scripts/TEST/wcm/DrainWater.cs
@@ -1,29 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DrainWater : MonoBehaviour
 {
-    public Vector3 targetScale; // 목표 크기
-    public float duration = 10f; // 변환 기간 (초)
-
     private Vector3 initialScale; // 초기 크기
+    private Vector3 targetScale = new Vector3(0.008f, 0.0015f, 0.035f); // 목표 크기
+
+    public float duration = 10f; // 변환 기간 (초)
     private float startTime; // 시작 시간
 
     private Vector3 initialPosition;
 
     private void Start()
     {
-        initialScale = transform.localScale; // 초기 크기 저장
-        initialPosition = transform.position;
-        targetScale = new Vector3(0.014f, 0.001f, 0.038f);
-        // 코루틴 시작
+        initialScale = transform.localScale;  // 초기 크기 저장
+        initialPosition = transform.position; // 초기 위치 저장
+        // 코루틴 시작   
+        
+    }
+
+    public void StartDrain()
+    {
         StartCoroutine(ChangeSizeOverTime());
     }
 
     private IEnumerator ChangeSizeOverTime()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.1f);
         float elapsedTime = 0f;
         startTime = Time.time; // 시작 시간 저장
         while (elapsedTime < duration)
@@ -34,7 +39,7 @@ public class DrainWater : MonoBehaviour
             // Lerp를 사용하여 크기 변경
             float t = elapsedTime / duration;
 
-            transform.position = Vector3.Lerp(initialPosition, initialPosition + new Vector3(0, 0.484f, 0), t);
+            transform.position = Vector3.Lerp(initialPosition, initialPosition + new Vector3(0, -0.308f, 0), t);
             transform.localScale = Vector3.Lerp(initialScale, targetScale, t);
 
             yield return null;
