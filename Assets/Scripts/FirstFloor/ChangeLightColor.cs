60ac3b19b7821db87fe1df6ac2bd9046ae83aebe
인권 백 <baek626@gmail.com>
2023-09-14 03:18:48 +0900

    chore : Modify for NullException

--- Assets/Scripts/FirstFloor/ChangeLightColor.cs
+++ Assets/Scripts/FirstFloor/ChangeLightColor.cs
@@ -14,6 +14,7 @@ public class ChangeLightColor : MonoBehaviour
     {
         if (GameManager.Gimmick.UnderTo1F)
         {
+            if(light != null)
             light.color = Color.red;
         }
     }
