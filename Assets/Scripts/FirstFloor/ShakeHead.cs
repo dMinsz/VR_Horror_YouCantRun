aa3bb102c37d175d68e2b30b3c53c2c44f900ade
인권 백 <titanbaek91@gmail.com>
2023-09-14 17:58:51 +0900

    chore : Modify the headbanging script (serialized)

--- Assets/Scripts/FirstFloor/ShakeHead.cs
+++ Assets/Scripts/FirstFloor/ShakeHead.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class ShakeHead : MonoBehaviour
 {
+
+    [SerializeField] float headShakePower;
+    float rotateRange;
+
+    private void Start()
+    {
+        rotateRange = headShakePower > 0 ? headShakePower : 4;
+    }
     private void Update()
     {
         //transform.localRotation = Quaternion.Euler(0, -90, 0);
     }
     private void FixedUpdate()
     {
-        transform.localRotation = Quaternion.Euler(0, Random.Range(transform.localRotation.y - 4, transform.localRotation.y + 4), 0);
+        transform.localRotation = Quaternion.Euler(0, Random.Range(transform.localRotation.y - rotateRange, transform.localRotation.y + rotateRange), 0);
     }
 }
