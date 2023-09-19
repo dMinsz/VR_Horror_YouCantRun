080fddccadc2b8ddb4e2b618577184261bd69a0b
인권 백 <baek626@gmail.com>
2023-09-20 05:37:09 +0900

    chore : Delete Error code

--- Assets/Scripts/Fade/FadeSetCamera.cs
+++ Assets/Scripts/Fade/FadeSetCamera.cs
@@ -4,25 +4,5 @@ using UnityEngine;
 
 public class FadeSetCamera : MonoBehaviour
 {
-    Coroutine mainCamCoroutine;
-    Canvas fadeCanvas;
 
-    private void Awake()
-    {
-        fadeCanvas = GetComponent<Canvas>();
-    }
-    public void Start()
-    {
-        mainCamCoroutine = StartCoroutine(FindMainCamera());
-    }
-
-    IEnumerator FindMainCamera()
-    {
-        yield return new WaitUntil(() => { return Camera.main != null; });
-
-        mainCam = Camera.main.gameObject;
-        Debug.Log($"{gameObject.name} : MainCam Ã£À½");
-
-        yield break;
-    }
 }
